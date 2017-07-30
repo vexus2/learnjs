402bd26dd9bcf85c324ab79284906b6b6685033e
vexus2 <rinrin900@gmail.com>
2017-07-30 21:04:40 +0900

    2.4終了

--- public/app.js
+++ public/app.js
@@ -1,17 +1,18 @@
 'use strict';
 var learnjs = {};
 
-learnjs.problemView = function () {
-  return $('<div class="problem-view">').text('Coming soon!');
+learnjs.problemView = function (problemNumber) {
+  var title = 'Problem #' + problemNumber + ' Coming soon!';
+  return $('<div class="problem-view">').text(title);
 };
 
 learnjs.showView = function (hash) {
   var routes = {
-    '#problem-1' : learnjs.problemView
+    '#problem' : learnjs.problemView
   };
-  var viewFn = routes[hash];
+  var hashParts = hash.split('-');
+  var viewFn = routes[hashParts[0]];
   if (viewFn) {
-    $('.view-container').empty().append(viewFn());
+    $('.view-container').empty().append(viewFn(hashParts[1]));
   }
 };
-
--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -1,4 +1,4 @@
-describe('LeanJS', function () {
+describe('LearnJS', function () {
   it('can show a problem view', function () {
     learnjs.showView('#problem-1');
     expect($('.view-container .problem-view').length).toEqual(1);
@@ -6,5 +6,17 @@ describe('LeanJS', function () {
   it('shows the landing page view when there is no hash', function () {
     learnjs.showView('');
     expect($('.view-container .landing-view').length).toEqual(1);
+  });
+  it('passes the hash view parameter to the view function', function () {
+    spyOn(learnjs, 'problemView');
+    learnjs.showView('#problem-42');
+    expect(learnjs.problemView).toHaveBeenCalledWith('42');
+  });
+
+  describe('problem view', function () {
+    it('has a title that includes the problem number', function () {
+      var view = learnjs.problemView('1');
+      expect(view.text()).toEqual('Problem #1 Coming soon!');
+    });
   });
 });
